4808baf0b736133004f251217abd2ddd57e94288
Shumkarbek Barpyev <74590123+shumkar-barpiev@users.noreply.github.com>
2026-07-25 21:33:52 +0200

    vehicle mapper, services and implementations (#6)

---
+++ src/main/java/com/myexam/parkingsystem/service/implementation/VehicleServiceImpl.java
@@ -0,0 +1,86 @@
+package com.myexam.parkingsystem.service.implementation;
+
+import com.myexam.parkingsystem.config.global.exception.ConflictException;
+import com.myexam.parkingsystem.dto.vehicle.CreateVehicleRequest;
+import com.myexam.parkingsystem.dto.vehicle.UpdateVehicleRequest;
+import com.myexam.parkingsystem.dto.vehicle.VehicleResponse;
+import com.myexam.parkingsystem.entity.Vehicle;
+import com.myexam.parkingsystem.mapper.VehicleMapper;
+import com.myexam.parkingsystem.repository.VehicleRepository;
+import com.myexam.parkingsystem.service.VehicleService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class VehicleServiceImpl implements VehicleService {
+	private final VehicleRepository vehicleRepository;
+	private final VehicleMapper vehicleMapper;
+
+	VehicleServiceImpl(VehicleRepository vehicleRepository, VehicleMapper vehicleMapper) {
+		this.vehicleMapper = vehicleMapper;
+		this.vehicleRepository = vehicleRepository;
+	}
+
+	@Override
+	public VehicleResponse createVehicle(CreateVehicleRequest vehicleRequest) {
+		Vehicle vehicle = vehicleMapper.toEntity(vehicleRequest);
+		boolean vehicleExists = vehicleRepository.existsByLicensePlateIgnoreCase(vehicle.getLicensePlate());
+
+		if (vehicleExists) {
+			throw new ConflictException(
+					"A vehicle with this license plate already exists"
+			);
+		}
+
+		vehicleRepository.save(vehicle);
+		return vehicleMapper.toResponse(vehicle);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public VehicleResponse getVehicleById(Long id) {
+		Vehicle vehicle = vehicleRepository.findById(id).orElse(null);
+
+		if (vehicle == null) {
+			throw new ConflictException(
+					"Vehicle with id " + id + " not found"
+			);
+		}
+
+		return vehicleMapper.toResponse(vehicle);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<VehicleResponse> getAllVehicles() {
+		List<Vehicle> vehicles = vehicleRepository.findAll();
+		return vehicles.stream()
+				.map(vehicleMapper::toResponse)
+				.toList();
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public VehicleResponse updateVehicle(Long id, UpdateVehicleRequest request) {
+		Vehicle vehicle = vehicleRepository.findById(id).orElse(null);
+
+		if (vehicle == null) {
+			throw new ConflictException(
+					"Vehicle with id " + id + " not found"
+			);
+		}
+
+		vehicleMapper.updateEntity(vehicle, request);
+		vehicleRepository.save(vehicle);
+
+		return vehicleMapper.toResponse(vehicle);
+	}
+
+	@Override
+	public void deleteVehicle(Long id) {
+		Vehicle vehicle = vehicleRepository.findById(id).orElse(null);
+		vehicleRepository.delete(vehicle);
+	}
+}
