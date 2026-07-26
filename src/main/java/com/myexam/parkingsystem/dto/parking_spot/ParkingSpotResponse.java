721f24e206c1c5704dddc906481678a15f3cd405
Shumkarbek Barpyev <74590123+shumkar-barpiev@users.noreply.github.com>
2026-07-26 10:23:26 +0200

    create parking spot dto, mapper (#8)

---
+++ src/main/java/com/myexam/parkingsystem/dto/parking_spot/ParkingSpotResponse.java
@@ -0,0 +1,17 @@
+package com.myexam.parkingsystem.dto.parking_spot;
+
+import com.myexam.parkingsystem.entity.type.ParkingSpotType;
+import lombok.Value;
+
+import java.io.Serializable;
+
+/**
+ * DTO for {@link com.myexam.parkingsystem.entity.ParkingSpot}
+ */
+@Value
+public class ParkingSpotResponse implements Serializable {
+	Long id;
+	String spotNumber;
+	ParkingSpotType type;
+	boolean active;
+}
